d034d64f94cdf7d689a286647aeed8a47e0b3739
Galbatrollix <78318978+Galbatrollix@users.noreply.github.com>
2023-03-24 14:17:30 +0100

    added model filename parsing for input shape and file selection

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 import data_gen_new
-import model
+import utils
+import os
 import tensorflow as tf
 
 if __name__ == '__main__':
@@ -9,12 +10,16 @@ if __name__ == '__main__':
     IMAGES_TEST_PATH = "data/test"
     IMAGES_TRAIN_PATH = "data/train"
 
-    INPUT_SHAPE = (224, 224, 3)
+    MODELS_PATH = "models"
+
+    model_filename = utils.choose_file_to_load(MODELS_PATH)
+
+    input_shape = utils.model_filename_parse_dimension(model_filename)
 
     data_generator_train = data_gen_new.DataGenerator(img_dir_path=IMAGES_TRAIN_PATH,
                                                       labels_file_path=LABELS_TRAIN,
                                                       batch_size=25,
-                                                      target_dim=INPUT_SHAPE[:2],
+                                                      target_dim=input_shape,
                                                       arr_preprocessing_func=None,
                                                       img_preprocessing_func=None,
                                                       do_shuffling=False,
@@ -23,11 +28,12 @@ if __name__ == '__main__':
     data_generator_test = data_gen_new.DataGenerator(img_dir_path=IMAGES_TEST_PATH,
                                                      labels_file_path=LABELS_TEST,
                                                      batch_size=25,
-                                                     target_dim=INPUT_SHAPE[:2],
+                                                     target_dim=input_shape,
                                                      arr_preprocessing_func=None,
                                                      img_preprocessing_func=None,
                                                      do_shuffling=False,
                                                      do_augmentation=False)
 
-    model = tf.keras.models.load_model(filepath='models/model2.hp')
+    model = tf.keras.models.load_model(filepath=os.path.join(MODELS_PATH, model_filename))
+
     model.evaluate(x=data_generator_train, verbose=2)
