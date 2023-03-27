01d7ff08f3e2cfafe38fc9e28ee97dbdc0da4bb8
Galbatrollix <78318978+Galbatrollix@users.noreply.github.com>
2023-03-27 16:03:55 +0200

    improved confusion matrix image generation

--- evaluate.py
+++ evaluate.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
 
     MODELS_PATH = "models"
     REPORTS_PATH = "metrics/reports"
+    CONFUSION_MATRIX_PATH = "metrics/confusion_matrix"
 
     model_filename = utils.choose_file_to_load(MODELS_PATH)
 
@@ -44,7 +45,7 @@ if __name__ == '__main__':
                                                      do_augmentation=False)
 
     model = tf.keras.models.load_model(filepath=os.path.join(MODELS_PATH, model_filename))
-    #model.evaluate(x=data_generator_test, verbose=2)
+    # model.evaluate(x=data_generator_test, verbose=2)
 
     y_predict = model.predict(x=data_generator_test)
     y_predict_argmax = tf.argmax(input=y_predict, axis=1)
@@ -55,16 +56,26 @@ if __name__ == '__main__':
                                    y_pred=y_predict_argmax,
                                    target_names=target_names,
                                    output_dict=True)
-    pprint.pprint(report)
+    # pprint.pprint(report)
 
     report_filename = "clfreport_" + os.path.splitext(model_filename)[0] + ".json"
     with open(os.path.join(REPORTS_PATH, report_filename), "w") as outfile:
         json.dump(report, outfile, indent=2)
 
     # confiusion matrix
+    cm_filename = "ConfusionMatrix_" + os.path.splitext(model_filename)[0] + ".png"
     cm = confusion_matrix(list(labels_test.values()), y_predict_argmax)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot(include_values=False, xticks_rotation='vertical')
-    matplotlib.pyplot.show()
-    print('test')
 
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+
+    fig = matplotlib.pyplot.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+
+    disp.plot(include_values=False, xticks_rotation='vertical', ax=ax)
+
+    ax.set_xticks([x*10 for x in range(10)])
+    ax.set_yticks([x*10 for x in range(10)])
+
+    matplotlib.pyplot.savefig(os.path.join(CONFUSION_MATRIX_PATH, cm_filename),dpi=300)
+
+    matplotlib.pyplot.show()
