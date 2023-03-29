9cd17e339ad29f877b9aa540fe5086f9814e2846
Matthias <malichtenegger@gmx.at>
2023-03-29 17:23:22 +0200

    added models, still need finetuning

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 
-
-
 def makeModel(input_shape):
     model = tf.keras.Sequential()
     model.add(tf.keras.Input(shape=input_shape))
@@ -28,7 +26,6 @@ def makeModel(input_shape):
     loss_fun = tf.keras.losses.CategoricalCrossentropy()
     model.compile(loss=loss_fun, metrics=['accuracy'])
     return model
-
 
 def makeModel2(input_shape):
     model = tf.keras.Sequential()
@@ -58,6 +55,69 @@ def makeModel2(input_shape):
 
     return model
 
+def makeModelVGG16(input_shape):
+    #simple VGG16 stump as described in https://keras.io/api/applications/vgg/
+    model = tf.keras.applications.VGG16(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=input_shape, #input needs to be preprocessed
+    pooling="avg",
+    classes=102,
+    classifier_activation="softmax",
+    )
+
+    loss_fun = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(loss=loss_fun, metrics=['accuracy'])
+    return model
+
+def makeModelVGG19(input_shape):
+    #simple VGG16 stump as described in https://keras.io/api/applications/vgg/
+    model = tf.keras.applications.VGG19(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=input_shape, #input needs to be preprocessed
+    pooling="avg",
+    classes=102,
+    classifier_activation="softmax",
+    )
+
+    loss_fun = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(loss=loss_fun, metrics=['accuracy'])
+    return model
+
+def makeModelInceptionResNetV2(input_shape):
+    #simple VGG16 stump as described in https://keras.io/api/applications/inceptionresnetv2/
+    model = tf.keras.applications.InceptionResNetV2(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=input_shape, #input needs to be preprocessed
+    pooling="avg",
+    classes=102,
+    classifier_activation="softmax",
+    )
+
+    loss_fun = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(loss=loss_fun, metrics=['accuracy'])
+    return model
+
+def makeModelDenseNet121(input_shape):
+    #simple VGG16 stump as described in https://keras.io/api/applications/densenet/
+    model = tf.keras.applications.DenseNet121(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=input_shape, #input needs to be preprocessed
+    pooling="avg",
+    classes=102,
+    classifier_activation="softmax",
+    )
+
+    loss_fun = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(loss=loss_fun, metrics=['accuracy'])
+    return model
 
 if __name__ == '__main__':
     pass
